e4a85ac0a86d97e0a97e2d9eef12eea244a2edb8
Pintu kumar <pintu1012kumar@gmail.com>
2024-12-27 23:03:31 +0530

    backend updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,15 +14,4 @@ app.route('/api/v1/blog', blogRouter);
 
 
 
-// app.get('/check-env', (c) => {
-//   const databaseUrl = c.env.DATABASE_URL;
-//   const jwtSecret = c.env.JWT_SECRET;
-
-//   if (!databaseUrl || !jwtSecret) {
-//     return c.json({ message: 'Environment variables are missing' }, 500);
-//   }
-
-//   return c.json({ message: 'Environment variables are available', databaseUrl, jwtSecret });
-// });
-
 export default app;
